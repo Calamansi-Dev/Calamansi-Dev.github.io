8339819f71a377b30c9dcc1a46cb31a0bbb71c3b
neima yeganeh <nyeganeh@calpoly.edu>
2022-03-23 18:22:18 -0700

    added menu

--- Calamansi/src/app/app.module.ts
+++ Calamansi/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarouselComponent
+    CarouselComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
